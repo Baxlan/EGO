Fixes in data scaling. Now, only one isotropy is used in optimal metric searching. Now, model space is in [0,1] in all dimensions

BayesianOptim.py
@@ -97,18 +97,22 @@ def preprocess(x, data_info):  # x, never y
         # linearize log scaled variables
         if data_info[i][2] == "log":
             sign = 1
+            inf = data_info[i][3][0]
+            sup = data_info[i][3][1]
             if data_info[i][3][0] < 0:
                 sign = -1
-            inp[:, i] = np.ln(sign*inp[:, i])
+                inf = -data_info[i][3][1]
+                sup = -data_info[i][3][0]
+            inp[:, i] = np.log(sign*inp[:, i])
 
             # normalize log variables
-            inp[:, i] -= np.ln(sign*data_info[i][3][0])
-            inp[:, i] /= np.ln(sign*data_info[i][3][1])
+            inp[:, i] -= np.log(inf)
+            inp[:, i] /= np.log(sup)
 
         # normalize non log variables
         else:
             inp[:, i] -= data_info[i][3][0]
-            inp[:, i] /= data_info[i][3][1]
+            inp[:, i] /= (data_info[i][3][1] - data_info[i][3][0])
 
     return inp
 
@@ -121,24 +125,57 @@ def postprocess(x, data_info): # x, never y
     for i in range(len(data_info)):
         if data_info[i][2] == "log":
             sign = 1
+            inf = data_info[i][3][0]
+            sup = data_info[i][3][1]
             if data_info[i][3][0] < 0:
                 sign = -1
+                inf = -data_info[i][3][1]
+                sup = -data_info[i][3][0]
 
             # denormalize log variables
-            inp[:, i] *= np.ln(sign*data_info[i][3][1])
-            inp[:, i] += np.ln(sign*data_info[i][3][0])
+            inp[:, i] *= np.log(sup)
+            inp[:, i] += np.log(inf)
             # exponentiate log scaled variables
-            inp[:, i] = np.exp(sign*inp[:, i])
+            inp[:, i] = sign*np.exp(inp[:, i])
 
 
          # denormalize non log variables
         else:
-            inp[:, i] *= data_info[i][3][1]
+            inp[:, i] *= (data_info[i][3][1]-data_info[i][3][0])
             inp[:, i] += data_info[i][3][0]
 
     categorified = categorify_and_discretize_data(inp, data_info)
 
     return inp, categorified
+
+
+
+def check_data(x, y):
+    for i in range(x.shape[1]):
+        zero = False
+        one = False
+        for j in range(x.shape[0]):
+            if x[i, j] == 0:
+                zero = True
+            elif x[i, j] == 1:
+                one = True
+            elif x[i, j] < 0 or x[i, j] > 1:
+                raise Exception("Data have not been preprocessed")
+        if not zero or not one:
+            raise Exception("Data have not been preprocessed")
+
+    for i in range(y.shape[1]):
+        zero = False
+        one = False
+        for j in range(y.shape[0]):
+            if y[i, j] == 0:
+                zero = True
+            elif y[i, j] == 1:
+                one = True
+            elif y[i, j] < 0 or x[i, j] > 1:
+                raise Exception("Data have not been preprocessed")
+        if not zero or not one:
+            raise Exception("Data have not been preprocessed")
 
 
 
@@ -311,7 +348,15 @@ def optimized_metric(diffs, x, y, noise, isotropy, seed, initial, bounds, method
     if isotropy == "aniso":
         M = make_symmetric_matrix_from_list(M)
 
-    return delinearize_metric(x, M, bounds), -response.fun
+    M = delinearize_metric(x, M, bounds)
+
+    if isotropy != "aniso":
+        for i in range(M.shape[0]):
+            for j in range(M.shape[1]):
+                if i != j:
+                    M[i,j] = 0
+
+    return M, -response.fun
 
 
 
@@ -320,7 +365,7 @@ def optimized_metric_tuple(args):
 
 
 
-def optimal_metric(diffs, x, y, noise, bounds, seed, threads):
+def optimal_metric(diffs, x, y, noise, bounds, iso, seed, threads):
     if bounds[0] >= bounds[1]:
         raise Exception("Lower bound must be strictly inferior to upper bound")
 
@@ -328,16 +373,15 @@ def optimal_metric(diffs, x, y, noise, bounds, seed, threads):
     methods = ["gradient", "stochastic"]
     args = []
 
-    for iso, dim in isotropies.items():
-        n = 5 * dim * math.ceil(math.sqrt(dim))
-        m = math.ceil(math.log(n)/math.log(2))
-        pool = multiprocessing.Pool(threads)
-        generator = sp.stats.qmc.Sobol(d=dim, seed=seed)
-        for method in methods:
-            initial = generator.random_base2(m=m)[:n]
-            initial = [init*(bounds[1]-bounds[0])+bounds[0] for init in initial]
-            for init in initial:
-                args.append((diffs, x, y, noise, iso, seed, init, bounds, method))
+    n = 10 * isotropies[iso] * math.ceil(math.sqrt(isotropies[iso]))
+    m = math.ceil(math.log(n)/math.log(2))
+    pool = multiprocessing.Pool(threads)
+    generator = sp.stats.qmc.Sobol(d=isotropies[iso], seed=seed)
+    for method in methods:
+        initial = generator.random_base2(m=m)[:n]
+        initial = [init*(bounds[1]-bounds[0])+bounds[0] for init in initial]
+        for init in initial:
+            args.append((diffs, x, y, noise, iso, seed, init, bounds, method))
 
     metrics_lmls = pool.map(optimized_metric_tuple, args)
 
@@ -389,23 +433,42 @@ def bound_combinations(bounds):
 
 
 
+def scale_points(points, data_info):
+    res = []
+    for i in range(len(points)):
+        res.append(scale_point(points[i], data_info))
+
+    return np.array(res)
+
+
+
 def scale_point(point, data_info):
     for d in range(len(point)):
-        if data_info[d][0] == "lin":
-            point[d] *= (data_info[d][3] - data_info[d][2])
-            point[d] += data_info[d][2]
-        elif data_info[d][0] == "log":
-            point[d] = data_info[d][2] * math.exp(point[d] * math.log(data_info[d][3]/data_info[d][2]))
+        if data_info[d][2] == "log":
+            sign = 1
+            inf = data_info[d][3][0]
+            sup = data_info[d][3][1]
+            if data_info[d][3][0] < 0:
+                sign = -1
+                inf = -data_info[d][3][1]
+                sup = -data_info[d][3][0]
+
+            point[d] *= np.log(sup)
+            point[d] += np.log(inf)
+            point[d] = sign*np.exp(point[d])
+
+        else:
+            point[d] *= (data_info[d][3][1] - data_info[d][3][0])
+            point[d] += data_info[d][3][0]
 
     return point
 
 
 
 def first_points(data_info, n, seed):
-    points = random_points(data_info, n, seed)
-
+    points = random_points(data_info, n, seed) # already scaled
     bounds = bound_combinations([[0,1] for i in range(len(data_info))])
-    bounds = [scale_point(bound, data_info) for bound in bounds]
+    bounds = scale_points(bounds, data_info)
 
     return np.vstack([bounds, points])
 
@@ -519,17 +582,18 @@ def find_max_ei_gradient(args):
     response = sp.optimize.minimize( \
         fun=acquisition_function, x0=args[2], \
         args=(args[0], args[1], args[4], args[5], args[6]), method="L-BFGS-B", \
-        bounds=[[args[3][i][3][0], args[3][i][3][1]] for i in range(len(args[1][0]))])
+        bounds=[[0, 1] for i in range(len(args[1][0]))])
 
     return (-response.fun, response.x)
 
 
 
 def find_max_ei_stochastic(args):
+    print(args[2], flush=True)
     response = sp.optimize.differential_evolution( \
         func=acquisition_function, x0=args[2], seed=args[7], \
         args=(args[0], args[1], args[4], args[5], args[6]), \
-        bounds=[[args[3][i][3][0], args[3][i][3][1]] for i in range(len(args[1][0]))])
+        bounds=[[0, 1] for i in range(len(args[1][0]))])
 
     return (-response.fun, response.x)
 
@@ -587,19 +651,20 @@ def are_nested_contraint_satifcation_probable(value, sigma, a, constraints):
 
 
 class BayesianOptimizer:
-    def __init__(self, title, noise, data_info, constraints, seed, threads, epsilon=1e-13):
+    def __init__(self, title, noise, data_info, constraints, seed, threads, iso="diag", epsilon=1e-13):
         if np.array(noise).size != len(constraints)+1:
             raise Exception("Noise length must have same length than constraint one + 1 (ie: " + str(len(constraints)+1) + "). " + str(len(noise)) + " provided")
-        self._title = title
-        self._noise = noise
-        self._data_info = data_info
-        self._dummy_data_info = dummify_data_info(data_info)
-        self._constraints = constraints
-        self._seed = seed
-        self._threads = threads
-        self._x = None
-        self._y = None
-        self._epsilon = epsilon
+        self.title = title
+        self.noise = noise
+        self.data_info = data_info
+        self.dummy_data_info = dummify_data_info(data_info)
+        self.constraints = constraints
+        self.seed = seed
+        self.threads = threads
+        self.x = None
+        self.y = None
+        self.iso = iso
+        self.epsilon = epsilon
 
 
 
@@ -610,51 +675,53 @@ class BayesianOptimizer:
             raise Exception("X data must be 2-dimensional")
         if  y.ndim != 2:
             raise Exception("Y data must be 2-dimensional")
-        if y.shape[1] != len(self._constraints)+1:
-            raise Exception("Y data must containt as much data as constraints + 1 (ie: " + str(len(self._constraints)+1) + "). " + str(y.shape[1]) + " provided")
-        if self._x == None:
-            self._x = x
-            self._y = y
+        if y.shape[1] != len(self.constraints)+1:
+            raise Exception("Y data must containt as much data as constraints + 1 (ie: " + str(len(self.constraints)+1) + "). " + str(y.shape[1]) + " provided")
+        if self.x == None:
+            self.x = x
+            self.y = y
         else:
-            self._x = np.vstack([self._x, x])
-            self._y = np.vstack([self._y, y])
+            self.x = np.vstack([self.x, x])
+            self.y = np.vstack([self.y, y])
 
 
 
 
     def first_points(self, n):
-        self._seed += 1
-        return first_points(self._data_info, n, self._seed)
+        self.seed += 1
+        return first_points(self.data_info, n, self.seed)
 
 
 
     def next_points(self, n, a, metric_bounds=[-12, 12]):
-        x = preprocess(self._x, self._data_info)
+        x = preprocess(self.x, self.data_info)
 
-        self._seed += 1
+        self.seed += 1
         print("Calculating optimal metrics", flush=True)
         diffs = make_diff_list(x)
 
         metrics = []
         kernels = []
-        for i in range(len(self._constraints)+1):
-            metric, lml = optimal_metric(diffs, x, self._y[:,i], self._noise[i], metric_bounds, self._seed, self._threads)
+        for i in range(len(self.constraints)+1):
+            metric, lml = optimal_metric(diffs, x, self.y[:,i], self.noise[i], metric_bounds, self.iso, self.seed, self.threads)
             metrics.append(metric)
-            kernels.append(make_kernel(diffs, self._noise[i], metric))
+            kernels.append(make_kernel(diffs, self.noise[i], metric))
 
         print(metrics[0], flush=True)
+        print("lml = " + str(lml), flush=True)
         np.set_printoptions(formatter={'float':"{0:0.3f}".format})
         print(kernels[0], flush=True)
 
-        self._seed += 1
-        m = 5 * len(self._dummy_data_info) * math.ceil(math.sqrt(len(self._dummy_data_info)))
+        self.seed += 1
+        m = 100 * len(self.dummy_data_info) * math.ceil(math.sqrt(len(self.dummy_data_info)))
         print("Calculating next points", flush=True)
 
         models = []
         for i in range(len(metrics)):
-            models.append((kernels[i], self._y[:,i], metrics[i]))
+            models.append((kernels[i], self.y[:,i], metrics[i]))
 
-        next_pts = np.array(list(next_points(models, x, self._dummy_data_info, m, self._seed, a, self._epsilon, self._threads).values()))[:n]
+
+        next_pts = np.array(list(next_points(models, x, self.dummy_data_info, m, self.seed, a, self.epsilon, self.threads).values()))[:n]
         if n > next_pts.shape[0]-1:
             n =  next_pts.shape[0]-1
-        return postprocess(next_pts, self._dummy_data_info)
+        return postprocess(next_pts, self.dummy_data_info)

test_1D.py
@@ -30,7 +30,7 @@ if __name__ == '__main__':
 
 
     diffs = bo.make_diff_list(X_in)
-    metric, lml = bo.optimal_metric(diffs, X_in, y, noise=0, bounds=[-12, 12], seed=32, threads=6)
+    metric, lml = bo.optimal_metric(diffs, X_in, y, noise=0, bounds=[-12, 12], iso="iso", seed=32, threads=6)
 
     print(metric, flush=True)
     print(lml, flush=True)
@@ -44,14 +44,14 @@ if __name__ == '__main__':
     pred, sigma = bo.predict(model, X_in, X_out)
 
     X_out = X_out.reshape(1, -1)[0]
+    a = 5
 
     fig, ax1 = plt.subplots()
     ax1.plot(X_test, y_test, label="true function", c="black")
     ax1.scatter(X_in, y, label="first evaluations", c="blue")
     ax1.plot(X_out, pred, label="surrogate", c="orange")
-    ax1.fill(np.hstack([X_out, X_out[::-1]]), np.hstack([pred - 1.9600 * sigma, (pred + 1.9600 * sigma)[::-1]]), alpha = 0.5, fc = "b")
+    ax1.fill(np.hstack([X_out, X_out[::-1]]), np.hstack([pred - a * sigma, (pred + a * sigma)[::-1]]), alpha = 0.5, fc = "b")
 
-    a = 5
     ei = bo.expected_improvement(pred, sigma, max(y), a=a, epsilon=1e-13)
 
 

test_3D.py
@@ -3,14 +3,15 @@ import math
 import BayesianOptim as bo
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
 
 
 # name, real or discrete, lin or log, bounds
 data_info = [
-    ["var1", "real", "lin", [0, 100]],
-    ["var2", "real", "lin", [0, 100]],
-    ["var3", "real", "lin", [0, 100]]]
+    ["var1", "real", "log", [1, 100]],
+    ["var2", "real", "lin", [0.1, 100]],
+    ["var3", "real", "lin", [0.1, 100]]]
 
 
 
@@ -36,16 +37,35 @@ def func3(x):
 
 if __name__ == '__main__':
     random.seed(0)
-    X_in = []
 
-    for i in range(10):
-        X_in.append([random.uniform(0, 100), random.uniform(0, 100), random.uniform(0, 100)])
-    X_in = np.array(X_in)
+    if True:
+        X_in = bo.first_points(data_info, 20, 0)
+    else:
+        X_in = []
+        for i in range(200):
+            X_in.append([random.uniform(0, 100), random.uniform(0, 100), random.uniform(0, 100)])
+        X_in = np.array(X_in)
 
     y = [func3(x) for x in X_in]
 
     # test BayesianOptimiser class
-    optimizer = bo.BayesianOptimizer("test", noise=[0], data_info=data_info, constraints=[], seed=32, threads=6)
+    optimizer = bo.BayesianOptimizer("test", noise=[0], data_info=data_info, constraints=[], iso="diag", seed=32, threads=7)
     optimizer.add_data(X_in, np.array(y).reshape(-1,1))
     next_points = optimizer.next_points(n=10, a=10)
     print(next_points, flush=True)
+
+    X_plot = []
+    for i in range(10000):
+        #X_plot.append([i/10, 17.636, 93.36])
+        X_plot.append([i/100, X_in[0, 1], X_in[0, 2]])
+    X_plot = np.array(X_plot)
+
+    model = [optimizer.kernel, y, optimizer.metric]
+    y_pred, y_sigma = bo.predict(model, X_in, X_plot)
+
+    a = 1
+    plt.plot(X_plot[:, 0], y_pred)
+    plt.fill(np.hstack([X_plot[:, 0], X_plot[:, 0][::-1]]), np.hstack([y_pred - a * y_sigma, (y_pred + a * y_sigma)[::-1]]), alpha = 0.5, fc = "b")
+
+    plt.grid()
+    plt.show()

test_constraint.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 # name, real or discrete, lin or log, bounds
 data_info = [
-    ["var1", "real", "lin", [0, 1]]]
+    ["var1", "real", "log", [0.01, 1]]]
 
 constraints = [
     ["title", [[">", 0.6, "<", 0.7], [">", 1]]]
@@ -37,8 +37,8 @@ if __name__ == '__main__':
 
 
     diffs = bo.make_diff_list(X_in)
-    metric, lml = bo.optimal_metric(diffs, X_in, y, noise=0, bounds=[-12, 12], seed=32, threads=6)
-    metric_constr, lml = bo.optimal_metric(diffs, X_in, y_constr, noise=0, bounds=[-12, 12], seed=32, threads=6)
+    metric, lml = bo.optimal_metric(diffs, X_in, y, noise=0, bounds=[-12, 12], iso="iso", seed=32, threads=6)
+    metric_constr, lml = bo.optimal_metric(diffs, X_in, y_constr, noise=0, bounds=[-12, 12], iso="iso", seed=32, threads=6)
 
     print(metric_constr, flush=True)
     np.set_printoptions(formatter={'float':"{0:0.3f}".format})
@@ -79,7 +79,7 @@ if __name__ == '__main__':
     ax2 = ax1.twinx()
     ax2.plot(X_out, ei, label="expected improvement", c="cyan")
 
-    next_pt = bo.next_points([model, model_constr], X_in, data_info, constraints=constraints, n=10, seed=32, a=a, threads=6)
+    next_pt = bo.next_points([model, model_constr], X_in, data_info, constraints=constraints, n=10, a=a, seed=32, threads=6)
     X2_in = list(next_pt.values())[0][0]
     y2 = func(X2_in)
     ax1.scatter(X2_in, y2, label="next point", c="red", s=60)